d27ce9aff91277369b56de25891132f1befc862a
gabriel-labitcoinera <gabrielchavezriera@gmail.com>
2022-08-22 10:26:14 -0600

    Add home page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,29 +1,5 @@
-//Matrix fuctionality
-const darkTheme = {
-  bgColor: 'rgba(0,0,0, 0.05)',
-  color: '#22b455',
-  chars: undefined,
-  fontSize: 14
-};
-const lightTheme = {
-  bgColor: 'rgba(255,255,255,0.05)',
-  color: '#000',
-  chars: ['0', '1'],
-  fontSize: 16
-};
-
-let backgroundColor = darkTheme.bgColor;
-let color = darkTheme.color;
-let chars = darkTheme.chars;
-let fontSize = darkTheme.fontSize;
-
-const canvas = document.getElementById('canvas');
-let matrixCanvas = matrix(canvas, {
-  chars: chars,
-  color: color,
-  background: backgroundColor,
-  font_size: fontSize
-});
+//Typewriter
+let skipped = false;
 
 //Internationalization fuctionality
 let languageConfig;
@@ -40,7 +16,7 @@ const replaceText = (element, lang) => {
 };
 
 const changeLanguage = (event) => {
-  option = event.target.value;
+  const option = event.target.value || event.target.getAttribute('value');
 
   const elements = document.querySelectorAll('[data-i18n]');
   elements.forEach(el => replaceText(el, option));
@@ -49,56 +25,60 @@ const changeLanguage = (event) => {
 const languageFilter = document.getElementById("language");
 languageFilter.addEventListener('change', changeLanguage);
 
-//Theming fuctionality
-const updateCanvas = () => {
-  //Stop matrix canvas
-  window.dispatchEvent(new KeyboardEvent('keydown', {
-    'key': 'q'
-  }));
-  //Change matrix canvas
-  matrixCanvas.then(() =>{
-    matrixCanvas = matrix(canvas, {
-      chars: chars,
-      color: color,
-      background: backgroundColor,
-      font_size: fontSize
-    });
-  });
+const selectLanguage = (event) => {
+  //CHANGE LANGUAGE
+  const option = event.target.value || event.target.getAttribute('value');
+
+  const elements = document.querySelectorAll('[data-i18n]');
+  elements.forEach(el => replaceText(el, option));
+
+  document.getElementById("language").value = option;
+
+  //HIDE OPTIONS
+  document.getElementById("options").style.display = 'none';
+
+  //START TYPEWRITING THE TEXT
+  let first = tply.animate(
+    document.getElementById("source"),
+    document.getElementById("destination")
+  );
+  let second = tply.animate(
+    document.getElementById("source-1"),
+    document.getElementById("destination-1")
+  );
+  setTimeout(
+    function() {
+      document.querySelector('#destination .cursor').style.display='none';
+    }, 17000);
+  
+  setTimeout(
+    function() {
+      if (!skipped) {
+        document.querySelector('#pills').style.opacity='1';
+      }
+    }, 15000);
+
+  //START SKIP FUNCTIONALITY
+  const skip = () => {
+    first.cancel();
+    second.cancel();
+    document.querySelector('#pills').style.opacity='1';
+    document.querySelector('#destination').style.display='none';
+    document.querySelector('#destination-1').style.display='none';
+    document.querySelector('#source-show').style.display='block';
+    document.querySelector('#source-show-1').style.display='block';
+    document.querySelector('#skip').style.display='none';
+    document.getElementById("language").removeAttribute('disabled');
+  };
+  document.getElementById("skip").addEventListener('click', skip);
+  document.getElementById("skip").style.display = 'block';
+
+  setTimeout(
+    function() {
+      document.getElementById("skip").style.display = 'none';
+      document.getElementById("language").removeAttribute('disabled');
+    }, 51000);
 };
 
-const changeTheme = (event) => {
-  option = event.target.value;
-  backgroundColor = option === 'light' ? lightTheme.bgColor : darkTheme.bgColor;
-  color = option === 'light' ? lightTheme.color : darkTheme.color;
-  chars = option === 'light' ? lightTheme.chars : darkTheme.chars;
-  fontSize = option === 'light' ? lightTheme.fontSize : darkTheme.fontSize;
-  if (!document.body.classList.contains(option)) {
-    document.body.classList = [];
-    document.body.classList.add(option);
-    updateCanvas();
-  }
-};
-
-const themeFilter = document.getElementById("theme");
-themeFilter.addEventListener('change', changeTheme);
-
-//Menu fuctionality
-const menuBtn = document.getElementById("menuBtn");
-menuBtn.addEventListener('click', function() {
-  this.classList.toggle("change");
-  document.getElementById("menu").classList.toggle("change");
-});
-
-const menuLinks = document.querySelectorAll('header nav ul li a');
-menuLinks.forEach((menuLink) => {
-  menuLink.addEventListener('click', function() {
-    document.getElementById("menu").classList.toggle("change");
-  });
-});
-
-//R key functionality to restart the canvas
-window.addEventListener('keydown', function(event) {
-  if (event.key.toLowerCase() === 'r') {
-    updateCanvas();
-  }
-});
+document.getElementById("langBtnEn").addEventListener('click', selectLanguage);
+document.getElementById("langBtnEs").addEventListener('click', selectLanguage);
